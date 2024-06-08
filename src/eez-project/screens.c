dcd78ec9d46b35b807f2278189f02f3836b60112
Martin Vladic <martin.vladic@gmail.com>
2024-06-08 11:26:04 +0200

    update eez-framework

--- src/eez-project/screens.c
+++ src/eez-project/screens.c
@@ -2,65 +2,26 @@
 #include "images.h"
 #include "fonts.h"
 #include "actions.h"
+#include "vars.h"
 #include "styles.h"
 #include "ui.h"
 
 objects_t objects;
 lv_obj_t *tick_value_change_obj;
 
-static void event_handler_cb_main_page1_button(lv_event_t *e) {
+static void event_handler_cb_main_obj1(lv_event_t *e) {
     lv_event_code_t event = lv_event_get_code(e);
     void *flowState = e->user_data;
-    if (event == LV_EVENT_CLICKED) {
-        flowPropagateValue(flowState, 5, 1);
+    if (event == LV_EVENT_PRESSED) {
+        flowPropagateValue(flowState, 13, 0);
     }
 }
 
-static void event_handler_cb_main_page2_button(lv_event_t *e) {
+static void event_handler_cb_main_obj2(lv_event_t *e) {
     lv_event_code_t event = lv_event_get_code(e);
     void *flowState = e->user_data;
-    if (event == LV_EVENT_CLICKED) {
-        flowPropagateValue(flowState, 7, 1);
-    }
-}
-
-static void event_handler_cb_page1_home_button_1(lv_event_t *e) {
-    lv_event_code_t event = lv_event_get_code(e);
-    void *flowState = e->user_data;
-    if (event == LV_EVENT_CLICKED) {
-        flowPropagateValue(flowState, 1, 1);
-    }
-}
-
-static void event_handler_cb_page1_formatted_textarea(lv_event_t *e) {
-    lv_event_code_t event = lv_event_get_code(e);
-    void *flowState = e->user_data;
-    if (event == LV_EVENT_VALUE_CHANGED) {
-        lv_obj_t *ta = lv_event_get_target(e);
-        const char *value = lv_textarea_get_text(ta);
-        if (tick_value_change_obj != ta) {
-            assignStringProperty(flowState, 9, 2, value, "Failed to assign Text in Textarea widget");
-        }
-    }
-}
-
-static void event_handler_cb_page1_value_textarea(lv_event_t *e) {
-    lv_event_code_t event = lv_event_get_code(e);
-    void *flowState = e->user_data;
-    if (event == LV_EVENT_VALUE_CHANGED) {
-        lv_obj_t *ta = lv_event_get_target(e);
-        const char *value = lv_textarea_get_text(ta);
-        if (tick_value_change_obj != ta) {
-            assignStringProperty(flowState, 10, 2, value, "Failed to assign Text in Textarea widget");
-        }
-    }
-}
-
-static void event_handler_cb_page2_home_button_2(lv_event_t *e) {
-    lv_event_code_t event = lv_event_get_code(e);
-    void *flowState = e->user_data;
-    if (event == LV_EVENT_CLICKED) {
-        flowPropagateValue(flowState, 2, 1);
+    if (event == LV_EVENT_PRESSED) {
+        flowPropagateValue(flowState, 15, 0);
     }
 }
 
@@ -69,100 +30,74 @@ void create_screen_main() {
     lv_obj_t *obj = lv_obj_create(0);
     objects.main = obj;
     lv_obj_set_pos(obj, 0, 0);
-    lv_obj_set_size(obj, 240, 320);
+    lv_obj_set_size(obj, 320, 480);
     lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
     {
         lv_obj_t *parent_obj = obj;
         {
-            lv_obj_t *obj = lv_obj_create(parent_obj);
-            lv_obj_set_pos(obj, 0, 0);
-            lv_obj_set_size(obj, 240, 320);
+            lv_obj_t *obj = lv_img_create(parent_obj);
+            lv_obj_set_pos(obj, 41, 164);
+            lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+            lv_img_set_src(obj, &img_logo_lvgl);
+            lv_obj_add_flag(obj, LV_OBJ_FLAG_ADV_HITTEST);
+            lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
+        }
+        {
+            // eee_logo
+            lv_obj_t *obj = lv_img_create(parent_obj);
+            objects.eee_logo = obj;
+            lv_obj_set_pos(obj, 50, 50);
+            lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+            lv_img_set_src(obj, &img_logo_eez);
+            lv_obj_add_flag(obj, LV_OBJ_FLAG_ADV_HITTEST);
+            lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
+        }
+        {
+            lv_obj_t *obj = lv_label_create(parent_obj);
+            objects.obj0 = obj;
+            lv_obj_set_pos(obj, 0, 80);
+            lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+            lv_label_set_text(obj, "");
+            lv_obj_set_style_text_font(obj, &lv_font_montserrat_32, LV_PART_MAIN | LV_STATE_DEFAULT);
+            lv_obj_set_style_align(obj, LV_ALIGN_CENTER, LV_PART_MAIN | LV_STATE_DEFAULT);
+        }
+        {
+            lv_obj_t *obj = lv_btn_create(parent_obj);
+            objects.obj1 = obj;
+            lv_obj_set_pos(obj, 41, 407);
+            lv_obj_set_size(obj, 100, 40);
+            lv_obj_add_event_cb(obj, event_handler_cb_main_obj1, LV_EVENT_ALL, flowState);
+            lv_obj_add_flag(obj, LV_OBJ_FLAG_SCROLL_ON_FOCUS);
             lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
             {
                 lv_obj_t *parent_obj = obj;
                 {
                     lv_obj_t *obj = lv_label_create(parent_obj);
-                    lv_obj_set_pos(obj, 2, -15);
+                    lv_obj_set_pos(obj, 0, 0);
                     lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-                    lv_label_set_text(obj, "EEZ Studio LVGL GUI builder");
+                    lv_label_set_text(obj, "+");
+                    lv_obj_set_style_text_font(obj, &lv_font_montserrat_48, LV_PART_MAIN | LV_STATE_DEFAULT);
+                    lv_obj_set_style_align(obj, LV_ALIGN_CENTER, LV_PART_MAIN | LV_STATE_DEFAULT);
                 }
+            }
+        }
+        {
+            lv_obj_t *obj = lv_btn_create(parent_obj);
+            objects.obj2 = obj;
+            lv_obj_set_pos(obj, 181, 407);
+            lv_obj_set_size(obj, 100, 40);
+            lv_obj_add_event_cb(obj, event_handler_cb_main_obj2, LV_EVENT_ALL, flowState);
+            lv_obj_add_flag(obj, LV_OBJ_FLAG_SCROLL_ON_FOCUS);
+            lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
+            {
+                lv_obj_t *parent_obj = obj;
                 {
                     lv_obj_t *obj = lv_label_create(parent_obj);
-                    lv_obj_set_pos(obj, 12, 1);
+                    lv_obj_set_pos(obj, 0, 0);
                     lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-                    lv_label_set_text(obj, "EEZ flow animation demo");
-                    lv_obj_set_style_text_color(obj, lv_color_hex(0xffff0000), LV_PART_MAIN | LV_STATE_DEFAULT);
-                }
-                {
-                    lv_obj_t *obj = lv_label_create(parent_obj);
-                    lv_obj_set_pos(obj, -8, 17);
-                    lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-                    lv_label_set_text(obj, "Build system: Platformio ESP32");
-                }
-                {
-                    lv_obj_t *obj = lv_label_create(parent_obj);
-                    lv_obj_set_pos(obj, 73, 33);
-                    lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-                    lv_label_set_text(obj, "LVGL: 8.3");
-                    lv_obj_set_style_text_color(obj, lv_color_hex(0xffec1010), LV_PART_MAIN | LV_STATE_DEFAULT);
-                }
-                {
-                    // page1_button
-                    lv_obj_t *obj = lv_btn_create(parent_obj);
-                    objects.page1_button = obj;
-                    lv_obj_set_pos(obj, 2, 69);
-                    lv_obj_set_size(obj, 94, 34);
-                    lv_obj_add_event_cb(obj, event_handler_cb_main_page1_button, LV_EVENT_ALL, flowState);
-                    lv_obj_add_flag(obj, LV_OBJ_FLAG_SCROLL_ON_FOCUS);
-                    lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
-                    {
-                        lv_obj_t *parent_obj = obj;
-                        {
-                            lv_obj_t *obj = lv_label_create(parent_obj);
-                            lv_obj_set_pos(obj, 0, 0);
-                            lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-                            lv_label_set_text(obj, "Page 1");
-                            lv_obj_set_style_align(obj, LV_ALIGN_CENTER, LV_PART_MAIN | LV_STATE_DEFAULT);
-                        }
-                    }
-                }
-                {
-                    // page2_button
-                    lv_obj_t *obj = lv_btn_create(parent_obj);
-                    objects.page2_button = obj;
-                    lv_obj_set_pos(obj, 116, 69);
-                    lv_obj_set_size(obj, 94, 34);
-                    lv_obj_add_event_cb(obj, event_handler_cb_main_page2_button, LV_EVENT_ALL, flowState);
-                    lv_obj_add_flag(obj, LV_OBJ_FLAG_SCROLL_ON_FOCUS);
-                    lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
-                    {
-                        lv_obj_t *parent_obj = obj;
-                        {
-                            lv_obj_t *obj = lv_label_create(parent_obj);
-                            lv_obj_set_pos(obj, 0, 0);
-                            lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-                            lv_label_set_text(obj, "Page 2");
-                            lv_obj_set_style_align(obj, LV_ALIGN_CENTER, LV_PART_MAIN | LV_STATE_DEFAULT);
-                        }
-                    }
-                }
-                {
-                    // eez_logo
-                    lv_obj_t *obj = lv_img_create(parent_obj);
-                    objects.eez_logo = obj;
-                    lv_obj_set_pos(obj, -5, 130);
-                    lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-                    lv_img_set_src(obj, &img_logo_eez);
-                    lv_obj_add_flag(obj, LV_OBJ_FLAG_ADV_HITTEST);
-                    lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
-                }
-                {
-                    lv_obj_t *obj = lv_img_create(parent_obj);
-                    lv_obj_set_pos(obj, -5, 235);
-                    lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-                    lv_img_set_src(obj, &img_logo_lvgl);
-                    lv_obj_add_flag(obj, LV_OBJ_FLAG_ADV_HITTEST);
-                    lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
+                    lv_label_set_text(obj, "-");
+                    lv_obj_set_style_text_font(obj, &lv_font_montserrat_48, LV_PART_MAIN | LV_STATE_DEFAULT);
+                    lv_obj_set_style_align(obj, LV_ALIGN_CENTER, LV_PART_MAIN | LV_STATE_DEFAULT);
                 }
             }
         }
@@ -171,153 +106,15 @@ void create_screen_main() {
 
 void tick_screen_main() {
     void *flowState = getFlowState(0, 0);
-}
-
-void create_screen_page1() {
-    void *flowState = getFlowState(0, 1);
-    lv_obj_t *obj = lv_obj_create(0);
-    objects.page1 = obj;
-    lv_obj_set_pos(obj, 0, 0);
-    lv_obj_set_size(obj, 240, 320);
-    lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
     {
-        lv_obj_t *parent_obj = obj;
-        {
-            // home_button_1
-            lv_obj_t *obj = lv_btn_create(parent_obj);
-            objects.home_button_1 = obj;
-            lv_obj_set_pos(obj, 0, -20);
-            lv_obj_set_size(obj, 94, 34);
-            lv_obj_add_event_cb(obj, event_handler_cb_page1_home_button_1, LV_EVENT_ALL, flowState);
-            lv_obj_add_flag(obj, LV_OBJ_FLAG_SCROLL_ON_FOCUS);
-            lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
-            lv_obj_set_style_align(obj, LV_ALIGN_BOTTOM_MID, LV_PART_MAIN | LV_STATE_DEFAULT);
-            {
-                lv_obj_t *parent_obj = obj;
-                {
-                    lv_obj_t *obj = lv_label_create(parent_obj);
-                    lv_obj_set_pos(obj, 0, 0);
-                    lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-                    lv_label_set_text(obj, "Home");
-                    lv_obj_set_style_align(obj, LV_ALIGN_CENTER, LV_PART_MAIN | LV_STATE_DEFAULT);
-                }
-            }
-        }
-        {
-            // formatted_textarea
-            lv_obj_t *obj = lv_textarea_create(parent_obj);
-            objects.formatted_textarea = obj;
-            lv_obj_set_pos(obj, -1, 104);
-            lv_obj_set_size(obj, 240, 42);
-            lv_textarea_set_max_length(obj, 128);
-            lv_textarea_set_one_line(obj, true);
-            lv_textarea_set_password_mode(obj, false);
-            lv_obj_add_event_cb(obj, event_handler_cb_page1_formatted_textarea, LV_EVENT_ALL, flowState);
-            lv_obj_set_style_text_align(obj, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN | LV_STATE_DEFAULT);
-            lv_obj_set_style_text_font(obj, &lv_font_montserrat_20, LV_PART_MAIN | LV_STATE_DEFAULT);
-        }
-        {
-            // value_textarea
-            lv_obj_t *obj = lv_textarea_create(parent_obj);
-            objects.value_textarea = obj;
-            lv_obj_set_pos(obj, 0, 21);
-            lv_obj_set_size(obj, 241, 42);
-            lv_textarea_set_max_length(obj, 128);
-            lv_textarea_set_one_line(obj, true);
-            lv_textarea_set_password_mode(obj, false);
-            lv_obj_add_event_cb(obj, event_handler_cb_page1_value_textarea, LV_EVENT_ALL, flowState);
-            lv_obj_set_style_text_align(obj, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN | LV_STATE_DEFAULT);
-            lv_obj_set_style_text_font(obj, &lv_font_montserrat_20, LV_PART_MAIN | LV_STATE_DEFAULT);
-        }
-        {
-            lv_obj_t *obj = lv_label_create(parent_obj);
-            lv_obj_set_pos(obj, 0, 0);
-            lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-            lv_label_set_text(obj, "Float:");
-        }
-        {
-            lv_obj_t *obj = lv_label_create(parent_obj);
-            lv_obj_set_pos(obj, 0, 83);
-            lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-            lv_label_set_text(obj, "Formatted string:");
-        }
-    }
-}
-
-void tick_screen_page1() {
-    void *flowState = getFlowState(0, 1);
-    {
-        const char *new_val = evalTextProperty(flowState, 9, 2, "Failed to evaluate Text in Textarea widget");
-        const char *cur_val = lv_textarea_get_text(objects.formatted_textarea);
+        const char *new_val = evalTextProperty(flowState, 12, 3, "Failed to evaluate Text in Label widget");
+        const char *cur_val = lv_label_get_text(objects.obj0);
         if (strcmp(new_val, cur_val) != 0) {
-            tick_value_change_obj = objects.formatted_textarea;
-            lv_textarea_set_text(objects.formatted_textarea, new_val);
+            tick_value_change_obj = objects.obj0;
+            lv_label_set_text(objects.obj0, new_val);
             tick_value_change_obj = NULL;
         }
     }
-    {
-        const char *new_val = evalTextProperty(flowState, 10, 2, "Failed to evaluate Text in Textarea widget");
-        const char *cur_val = lv_textarea_get_text(objects.value_textarea);
-        if (strcmp(new_val, cur_val) != 0) {
-            tick_value_change_obj = objects.value_textarea;
-            lv_textarea_set_text(objects.value_textarea, new_val);
-            tick_value_change_obj = NULL;
-        }
-    }
-}
-
-void create_screen_page2() {
-    void *flowState = getFlowState(0, 2);
-    lv_obj_t *obj = lv_obj_create(0);
-    objects.page2 = obj;
-    lv_obj_set_pos(obj, 0, 0);
-    lv_obj_set_size(obj, 240, 320);
-    lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
-    {
-        lv_obj_t *parent_obj = obj;
-        {
-            // txtarea1
-            lv_obj_t *obj = lv_textarea_create(parent_obj);
-            objects.txtarea1 = obj;
-            lv_obj_set_pos(obj, 0, 0);
-            lv_obj_set_size(obj, 240, 41);
-            lv_textarea_set_max_length(obj, 128);
-            lv_textarea_set_one_line(obj, true);
-            lv_textarea_set_password_mode(obj, false);
-        }
-        {
-            lv_obj_t *obj = lv_keyboard_create(parent_obj);
-            objects._obj_keyboard_0 = obj;
-            lv_obj_set_pos(obj, 0, -78);
-            lv_obj_set_size(obj, 240, 163);
-        }
-        {
-            // home_button_2
-            lv_obj_t *obj = lv_btn_create(parent_obj);
-            objects.home_button_2 = obj;
-            lv_obj_set_pos(obj, 0, -20);
-            lv_obj_set_size(obj, 94, 34);
-            lv_obj_add_event_cb(obj, event_handler_cb_page2_home_button_2, LV_EVENT_ALL, flowState);
-            lv_obj_add_flag(obj, LV_OBJ_FLAG_SCROLL_ON_FOCUS);
-            lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
-            lv_obj_set_style_align(obj, LV_ALIGN_BOTTOM_MID, LV_PART_MAIN | LV_STATE_DEFAULT);
-            {
-                lv_obj_t *parent_obj = obj;
-                {
-                    lv_obj_t *obj = lv_label_create(parent_obj);
-                    lv_obj_set_pos(obj, 0, 0);
-                    lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-                    lv_label_set_text(obj, "Home");
-                    lv_obj_set_style_align(obj, LV_ALIGN_CENTER, LV_PART_MAIN | LV_STATE_DEFAULT);
-                }
-            }
-        }
-    }
-    lv_keyboard_set_textarea(objects._obj_keyboard_0, objects.txtarea1);
-}
-
-void tick_screen_page2() {
-    void *flowState = getFlowState(0, 2);
 }
 
 
@@ -327,16 +124,12 @@ void create_screens() {
     lv_disp_set_theme(dispp, theme);
     
     create_screen_main();
-    create_screen_page1();
-    create_screen_page2();
 }
 
 typedef void (*tick_screen_func_t)();
 
 tick_screen_func_t tick_screen_funcs[] = {
     tick_screen_main,
-    tick_screen_page1,
-    tick_screen_page2,
 };
 
 void tick_screen(int screen_index) {
--- src/eez-project/screens.h
+++ src/eez-project/screens.h
@@ -9,29 +9,20 @@ extern "C" {
 
 typedef struct _objects_t {
     lv_obj_t *main;
-    lv_obj_t *page1;
-    lv_obj_t *page2;
-    lv_obj_t *_obj_keyboard_0;
-    lv_obj_t *eez_logo;
-    lv_obj_t *formatted_textarea;
-    lv_obj_t *home_button_1;
-    lv_obj_t *home_button_2;
-    lv_obj_t *page1_button;
-    lv_obj_t *page2_button;
-    lv_obj_t *txtarea1;
-    lv_obj_t *value_textarea;
+    lv_obj_t *eee_logo;
+    lv_obj_t *obj0;
+    lv_obj_t *obj1;
+    lv_obj_t *obj2;
 } objects_t;
 
 extern objects_t objects;
 
+enum ScreensEnum {
+    SCREEN_ID_MAIN = 1,
+};
+
 void create_screen_main();
 void tick_screen_main();
-
-void create_screen_page1();
-void tick_screen_page1();
-
-void create_screen_page2();
-void tick_screen_page2();
 
 void create_screens();
 void tick_screen(int screen_index);
